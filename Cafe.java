d8df7f025f27e3977a59fee1b2ea88cd1be66aeb
Nargiz Akhmetova <nargiz36194@gmail.com>
2024-10-30 22:15:39 -0400

    commit

--- Cafe.java
+++ Cafe.java
@@ -1,12 +1,61 @@
-/* This is a stub for the Cafe class */
-public class Cafe {
+/**
+ * Represents a cafe - type of building where coffee is being sold.
+ */
+public class Cafe extends Building {
 
-    public Cafe() {
-        System.out.println("You have built a cafe: ☕");
+    private int nCoffeeOunces; // The number of ounces of coffee remaining in inventory
+    private int nSugarPackets; // The number of sugar packets remaining in inventory
+    private int nCreams; // The number of "splashes" of cream remaining in inventory
+    private int nCups; // The number of cups remaining in inventory
+
+    /**
+     * Constructs a new Cafe object with the given parameters.
+     *
+     * @param name               The name of the cafe.
+     * @param address            The address of the cafe.
+     * @param nFloors            The number of floors in the cafe.
+     * @param initialCoffeeOunces The initial amount of coffee in ounces.
+     * @param initialSugarPackets The initial number of sugar packets.
+     * @param initialCreams       The initial number of cream "splashes".
+     * @param initialCups         The initial number of cups.
+     */
+    public Cafe(String name, String address, int nFloors, int initialCoffeeOunces, int initialSugarPackets,
+                int initialCreams, int initialCups) {
+        super(name, address, nFloors);
+        this.nCoffeeOunces = initialCoffeeOunces;
+        this.nSugarPackets = initialSugarPackets;
+        this.nCreams = initialCreams;
+        this.nCups = initialCups;
     }
-    
-    public static void main(String[] args) {
-        new Cafe();
+
+    /**
+     * Sells a cup of coffee with the specified size and ingredients.
+     *
+     * @param size          The size of the coffee in ounces.
+     * @param nSugarPackets The number of sugar packets.
+     * @param nCreams       The number of cream "splashes".
+     */
+    public void sellCoffee(int size, int nSugarPackets, int nCreams) {
+        if (nCups > 0) { // Check if there are available cups before selling coffee
+            if (size <= nCoffeeOunces && nSugarPackets <= this.nSugarPackets && nCreams <= this.nCreams) {
+                this.nCoffeeOunces -= size;
+                this.nSugarPackets -= nSugarPackets;
+                this.nCreams -= nCreams;
+                this.nCups--;
+                System.out.println("Coffee sold successfully!");
+            } else {
+                System.out.println("Insufficient inventory to sell coffee.");
+                restock(size, nSugarPackets, nCreams, 1); // Restock with one cup
+            }
+        } else {
+            System.out.println("No cups available to sell coffee.");
+        }
     }
-    
+
+    private void restock(int nCoffeeOunces, int nSugarPackets, int nCreams, int nCups) {
+        this.nCoffeeOunces += nCoffeeOunces;
+        this.nSugarPackets += nSugarPackets;
+        this.nCreams += nCreams;
+        this.nCups += nCups;
+    }
 }
--- Library.java
+++ Library.java
@@ -1,12 +1,111 @@
-/* This is a stub for the Library class */
-public class Library {
+import java.util.Hashtable;
 
-    public Library() {
-      System.out.println("You have built a library: 📖");
+/**
+ * Represents a library - type of building where books are being stored.
+ */
+public class Library extends Building {
+    
+    private Hashtable<String, Boolean> collection;
+
+    /**
+     * Constructs a new Library object with the given parameters.
+     *
+     * @param name     The name of the library.
+     * @param address  The address of the library.
+     * @param nFloors  The number of floors in the library.
+     */
+    public Library(String name, String address, int nFloors) {
+        super(name, address, nFloors);
+        this.collection = new Hashtable<>();
     }
-  
-    public static void main(String[] args) {
-      new Library();
+
+    /**
+     * Adds a book title to the library's collection if it's not already present.
+     *
+     * @param title The title of the book to add.
+     */
+    public void addTitle(String title) {
+        if (!collection.containsKey(title)) {
+            collection.put(title, true);
+        } else {
+            System.out.println("The title is already in the collection.");
+        }
     }
-  
-  }
+
+    /**
+     * Removes a book title from the library's collection.
+     *
+     * @param title The title of the book to remove.
+     * @return The title of the book removed, or null if the book was not found.
+     */
+    public String removeTitle(String title) {
+        if (collection.remove(title) != null) {
+            return title;
+        }
+        return null;
+    }
+
+    /**
+     * Checks out a book from the library if it's available.
+     *
+     * @param title The title of the book to check out.
+     */
+    public void checkOut(String title) {
+        if (collection.containsKey(title)) {
+            if (collection.get(title)) {
+                collection.put(title, false);
+            } else {
+                System.out.println("The book is already checked out.");
+            }
+        } else {
+            System.out.println("The book is not in the collection.");
+        }
+    }
+
+    /**
+     * Returns a book to the library if it was checked out.
+     *
+     * @param title The title of the book to return.
+     */
+    public void returnBook(String title) {
+        if (collection.containsKey(title)) {
+            if (!collection.get(title)) {
+                collection.put(title, true);
+            } else {
+                System.out.println("The book is already returned.");
+            }
+        } else {
+            System.out.println("The book is not in the collection.");
+        }
+    }
+
+    /**
+     * Checks if the library contains a specific book title.
+     *
+     * @param title The title of the book to check.
+     * @return true if the book is in the collection, false otherwise.
+     */
+    public boolean containsTitle(String title) {
+        return collection.containsKey(title);
+    }
+
+    /**
+     * Checks if a book is available for checkout.
+     *
+     * @param title The title of the book to check.
+     * @return true if the book is available, false otherwise.
+     */
+    public boolean isAvailable(String title) {
+        return collection.getOrDefault(title, false);
+    }
+
+    /**
+     * Prints the library's collection with checkout status.
+     */
+    public void printCollection() {
+        for (String title : collection.keySet()) {
+            System.out.println(title + " - Available: " + collection.get(title));
+        }
+    }
+    
+}
